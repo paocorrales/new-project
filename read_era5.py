0673901f900f459e07025bbc45808cb5c2bb6750
Pao Corrales <paobcorrales@gmail.com>
2025-03-27 15:51:27 +1100

    add plot

--- read_era5.py
+++ read_era5.py
@@ -1,5 +1,13 @@
 import xarray as xr
+import matplotlib.pyplot as plt
 
 # Load a NetCDF climate dataset
 ds = xr.open_mfdataset("era5_data.nc")
 print(ds)
+
+# Plot a temperature time series
+plt.plot(df["date"], df["temperature"])
+plt.xlabel("Date")
+plt.ylabel("Temperature")
+plt.title("Temperature Over Time")
+plt.show()
